8a68ce282c22f4909b1045081e96932e5de1de30
David Lay <davidjlay2277@gmail.com>
2023-08-17 20:13:51 -0500

    add seed

--- server/ctrl.js
+++ server/ctrl.js
@@ -1,9 +1,11 @@
 //pull from database files
+require("dotenv").config();
+
 const something = require("./db.json");
 const somethingelse = require("./db.sql");
 
 module.exports = {
-  //add callbacks to this object that are use in requests on the server
+  //add callbacks to this object that are used in requests on the server
   function1: (req, res) => {
     console.log("hit on function 1 in the constoler");
   },
---
+++ server/seed.js
@@ -0,0 +1,9 @@
+//pull from database files
+require("dotenv").config();
+
+module.exports = {
+  //add callbacks to this object that are used in requests on the server
+  function1: (req, res) => {
+    console.log("hit on function 1 in the constoler");
+  },
+};
