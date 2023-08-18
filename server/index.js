79811e9e639b0b09a47f07ed01cc26fd55eb1a90
David Lay <davidjlay2277@gmail.com>
2023-08-17 20:10:14 -0500

    getting started

--- server/index.js
+++ server/index.js
@@ -6,8 +6,19 @@ run the following commands:
     npm i express
         package-lock.json installed
     npm i cors
-    nodemon server
-        
+
+    npm install dotenv
+        Create a file in the root called.env
+            Create a SERVER_PORT variable
+            Create a CONNECTION_STRING variable (currently using supabase)
+
+At the top of controller.js, require in the dotenv package and call its config method (you can ignore the other code in there until later steps)
+
+Optional: destructure CONNECTION_STRING from process.env in controller.js
+
+    nodemon 
+        run index js, spin up server
+
     then create git repository (see .gitignore for node_modules)
 *///////////////////////////////////////////////////////
 
