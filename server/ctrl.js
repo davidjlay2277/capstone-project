75fbc4cd6e270c19131e5128c2511f3c1b9309ad
David Lay <davidjlay2277@gmail.com>
2023-08-19 14:38:50 -0500

    seed game

--- server/ctrl.js
+++ server/ctrl.js
@@ -14,16 +14,28 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
 
 module.exports = {
   //add callbacks to this object that are used in requests on the server
-  seed: (req, res) => {
-   sequelize.query(
-    `CREATE TABLE cards
-      
-    `
-    )
-    .then(() => {
-      console.log("DE Seeded");
-      res.sendStatus(200);
-    })
-    .catch((err) => console.log("Error: DB not seeded", err));
+  seedGame: (req, res) => {
+    sequelize
+      .query(
+        `DROP TABLE IF EXISTS game;
+        CREATE TABLE game(
+          idGame SERIAL PRIMARY KEY
+          ,idUserCharacter INTEGER REFERENCES characters(idCharacter)
+          ,idBotCharacter INTEGER REFERENCES characters(idCharacter)
+          ,userHealth INTEGER
+          ,botHealth INTEGER
+          ,winner VARCHAR(100)
+        );
+          INSERT INTO game (idUserCharacter, idBotCharacter, winner) 
+          VALUES
+          (1,1,'inProgress');
+          
+    )`
+      )
+      .then(() => {
+        console.log("DB Seeded");
+        res.sendStatus(200);
+      })
+      .catch((err) => console.log("Error: DB not seeded", err));
   },
 };
--- server/index.js
+++ server/index.js
@@ -1,35 +1,16 @@
-/*//////////////////INITIALIZE SERVER ////////////////////
-install nodemon (nmp install -g nodemon)
-run the following commands:
-    npm init -y
-        package.json installed
-    npm i express
-        package-lock.json installed
-    npm i cors
-
-    npm install dotenv
-        Create a file in the root called.env
-            Create a SERVER_PORT variable
-            Create a CONNECTION_STRING variable (currently using supabase)
-            ctrl.js, require in the dotenv package and call its config method (you can ignore the other code in there until later steps)
-
-    npm install sequelize pg pg-hstore
-        controller.js, require the sequelize package and save it to a variable called Sequelize
-        Initialize a new Sequelize instance and pass in your connection string.
-
-    nodemon 
-        run index js, spin up server
-
-    then create git repository (see .gitignore for node_modules)
-*///////////////////////////////////////////////////////
+require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
-app.use(express.json());
+const cors = require("cors");
 app.use(cors());
+app.use(express.json());
 
-const PORT = 5001;
-    //DEFINE PORT//
+const {seedGame} = require("./ctrl.js")
+
+//BASE URL should be 'http://localhost:4477'
+app.post("/seed",seedGame)
+
+const PORT = 4477;
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
